e70f5b166ced7554959c1141898118a2cd04434f
DenebAguirre <58702022+DenebAguirre@users.noreply.github.com>
2020-06-26 14:38:02 -0500

    Finished minimun requirements

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -7,24 +7,26 @@
 function responsivesvg() {
 
     function defaultchart(){
-        //select, create, svg area
-        var svgArea = d3.select("#scatter").select("svg")
         //Make sure the area is empty 
+        var svgArea = d3.select("#scatter").select("svg")
+    
         if (!svgArea.empty()) {
             svgArea.remove();
+        }
+  
+         //Margin
+        var chartMargin = {
+            top: 30,
+            right: 50,
+            bottom: 30,
+            left: 50,
         }
 
         //Define the sizes of our svg charte
         var svgHeight = window.innerHeight;
         var svgWidth = window.innerWidth;
 
-        //Margin
-        var chartMargin = {
-            top: 30,
-            right: 30,
-            bottom: 30,
-            left: 30,
-        }
+      
 
         // Space for our chart
         var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
@@ -37,50 +39,80 @@ function responsivesvg() {
             .attr("height", svgHeight)
             .attr("width", svgWidth);
 
-        //Lets group the chart figures with a g element
+        //Lets group the chart figures and move them to right
         var chartGroup = svg.append("g")
             .attr("transform", `translate (${chartMargin.left}, ${chartMargin.top})`)
 
 
         d3.csv("assets/data/data.csv").then(function(healthdata){
-            console.log(healthdata);
+          console.log(healthdata)
+          
+
+        //I order to d3.extent to work I need to cast
+            healthdata.forEach(function(data) {
+                data.smokes = +data.smokes;
+                data.age = +data.age;
+            });
+
 
         //define scales towards the data 
-            var scaleY = d3.scaleLinear()
-                .domain(d3.extent(healthdata, d => d.age))
-                .range([0, svgHeight]);
-
             var scaleX = d3.scaleLinear()
-                .domain(d3.extent(healthdata, d => d.smokes))
-                .range([0, svgWidth]);
+                .domain(d3.extent(healthdata, d => d.age))
+                .range([ 0, svgHeight]);
+
+            var scaleY = d3.scaleLinear()
+                .domain(d3.extent(healthdata, d => d.smokes)) 
+                .range([svgWidth, 0]);
+               
 
         //Create axis
+
+            var Xaxis = d3.axisBottom(scaleX).ticks(10);
+            var Yaxis = d3.axisLeft(scaleY).ticks(10);
             
-            var Yaxis = d3.axisLeft(scaleY);
-            var Xaxis = d3.axisRight(scaleX);
+       // append axis
+            chartGroup.append("g")
+                .classed("x-axis", true)
+                .attr("transform", `translate(0, ${chartHeight})`)
+                .call(Xaxis);
 
-        //Appen the axis
-       // append x axis
-        chartGroup.append("g")
-            .classed("x-axis", true)
-            .attr("transform", `translate(0, ${chartHeight})`)
-            .call(Xaxis);
+            chartGroup.append("g")
+                .call(Yaxis);
 
-            // append y axis
-        chartGroup.append("g")
-            .call(Yaxis);
+        // append axis text
+            chartGroup.append("text")
+                .attr("transform", `translate(${chartWidth/2}, ${chartHeight + chartMargin.top +20})`)
+                .attr("stroke", "black")
+                .text("Age(Median)")
 
+            chartGroup.append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("x", chartMargin.right)
+                .attr("y", chartHeight/2)
+                .attr("stroke", "black")
+                .text("Smokes(%)")
 
-
+            
         //Add the svg circles 
-            chartGroup.selectAll("circle")
-                .data(healthdata)
+            var circles = chartGroup.selectAll("circle");
+
+            circles.data(healthdata)
                 .enter()
                 .append("circle")
-                .attr("cx", d => scaleX(d.smokes))
-                .attr("cy", d => scaleY(d.age))
-                .attr("r", 5)
+                .attr("cx", d => scaleX(d.age))
+                .attr("cy", d => scaleY(d.smokes))
+                .attr("r", 10)
                 .attr("fill", "purple")
+            //add text
+            circles.data(healthdata)
+                .enter()
+                .append("text")
+                .attr("x", d => scaleX(d.age))
+                .attr("y", d => scaleY(d.smokes))
+                .attr("fill", "white")
+                .attr("text-anchor", "middle")
+                .attr("font-size", "8px")
+                .text(d => d.abbr)
     
         });
     }
